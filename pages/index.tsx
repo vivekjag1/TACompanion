a778bd701c017fdfcc3fe457df5ae7ea8c3bac35
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-13 10:24:52 -0400

    hero page link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import QuadImage from "../public/wpi-full-quad.jpg";
 import Image from "next/image";
+import Link from "next/link";
 export default function Home(){
   return(
-    <>
+    <Link href="/todo">
     <div className="bg-white relative w-full h-screen overflow-hidden">
       <Image src={QuadImage} alt="WPI Quad" className="w-full h-screen z-0"/>
       <div className="absolute flex items-center justify-center inset-0 bg-gradient-to-b from-blue-500 to-blue-900 opacity-40 z-85"/>
@@ -13,6 +14,6 @@ export default function Home(){
 
     <h1 className="font-bold text-black text-center text-white font-mono text-4xl  animate-pulse mb-20">Click Anywhere to Get Started </h1>
   </div>
-    </>
+    </Link>
 )
 }
