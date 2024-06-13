7eb6f667d4d6cd4067d3b4c0f7842f4845106eb2
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-13 10:24:06 -0400

    finish hero page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
-import QuadImage from "../public/QuadImage.jpeg";
-import wpiWide from "../public/wpi_wide.jpg.jpeg";
+import QuadImage from "../public/wpi-full-quad.jpg";
 import Image from "next/image";
 export default function Home(){
   return(
+    <>
     <div className="bg-white relative w-full h-screen overflow-hidden">
-      <Image src={QuadImage} alt="WPI Quad" className="w-full h-screen"/>
-
-      <div className="absolute inset-0 bg-gradient-to-b from-blue-500 to-blue-900 opacity-40 z-85"></div>
+      <Image src={QuadImage} alt="WPI Quad" className="w-full h-screen z-0"/>
+      <div className="absolute flex items-center justify-center inset-0 bg-gradient-to-b from-blue-500 to-blue-900 opacity-40 z-85"/>
     </div>
-  )
+  <div className="absolute inset-0 flex flex-col justify-center items-center    z-20">
+    <h1 className="font-bold text-center text-white font-mono text-6xl ">Organize School, Work, and
+      free time -- all in one place </h1>
+
+    <h1 className="font-bold text-black text-center text-white font-mono text-4xl  animate-pulse mb-20">Click Anywhere to Get Started </h1>
+  </div>
+    </>
+)
 }
