869e0f873c322d5e07be8a7b099a5b6e868bc129
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-14 10:53:54 -0400

    new hero page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,51 @@
 import QuadImage from "../public/wpi-full-quad.jpg";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
-export default function Home(){
-  return(
-    <Link href="/todo">
-    <div className="bg-white relative w-full h-screen overflow-hidden">
-      <Image src={QuadImage} alt="WPI Quad" className="w-full h-screen z-0"/>
-      <div className="absolute flex items-center justify-center inset-0 bg-gradient-to-b from-blue-500 to-blue-900 opacity-40 z-85"/>
-    </div>
-  <div className="absolute inset-0 flex flex-col justify-center items-center  z-20">
-    <h1 className="font-bold text-center text-white font-mono text-6xl ">Organize School, Work, and
-      free time in one place </h1>
+import lib from "../public/lib.jpg";
+import park from "../public/park.jpg"
 
-    <h1 className="font-bold text-black text-center text-white font-mono text-4xl  animate-pulse mb-20">Click Anywhere to Get Started </h1>
-  </div>
+const Home: NextPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    //the entire hero section should link to the todolist section
+
+
+    <Link href="/todo">
+      <div className="bg-white relative w-full h-screen overflow-hidden">
+        <div className = "grid grid-cols-1 md:grid-cols-2">
+          <div className="relative h-screen">
+            <Image src={lib} alt={"library"}/>
+          </div>
+          <div className = "relative h-screen">
+            <Image src={park} alt = "park"/>
+          </div>
+        </div>
+
+
+
+
+
+
+        <Image src={QuadImage} alt="WPI Quad" className="w-full h-screen z-0" />
+        <div className="absolute flex items-center justify-center inset-0 bg-gradient-to-b from-blue-500 to-blue-900 opacity-40 z-10" />
+      </div>
+      <div className="absolute inset-0 flex flex-col justify-center items-center z-20">
+        <h1 className="font-bold text-center text-white font-mono text-6xl">
+          Organize School, Work, and free time in one place
+        </h1>
+        <h1 className="font-bold text-center text-white font-mono text-4xl animate-pulse mb-20">
+          Click Anywhere to Get Started
+        </h1>
+      </div>
     </Link>
-)
-}
+  );
+};
+
+// This page should be rendered statically, but there are no props so just return nothing in place of the props.
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+export default Home;
