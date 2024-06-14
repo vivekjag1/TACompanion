e8150c9b4d407d17a6608933b5b6ad0e032fd2b7
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-14 13:02:52 -0400

    redeploy

--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,7 +1,7 @@
 const Home = () =>{
   return (
     <div className="bg-white font-mono text-8xl text-center items-center justify-center h-screen text-black">
-      To be finished! (Test azure redeploy)
+      To be finished! (Test azure redeploy)!
     </div>
   )
 };
