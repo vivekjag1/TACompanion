0a2d5c1b796c243fb431bdf448f5b9036eda1a68
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-16 12:27:18 -0400

    fix bug that I just created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const
+
   return <Component {...pageProps} />
 }
