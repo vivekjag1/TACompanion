09dd48fbbffe11f8112f3fbcde75b7a4e493740d
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-16 21:45:38 -0400

    HOTFIX navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from 'next/router';
+import NavBar from "@/components/NavBar";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
 
-  return <Component {...pageProps} />
+  const isHero = () => router.pathname === "/";
+
+  return(
+    <>
+      {!isHero() && <NavBar/>}
+      <Component {...pageProps}/>
+    </>
+  )
 }
--- pages/todo.tsx
+++ pages/todo.tsx
@@ -2,12 +2,12 @@ import NavBar from "../components/NavBar";
 
 const Home = () =>{
   return (
-    <>
-      <NavBar/>
+
+
       <div className="bg-white font-mono text-8xl text-center items-center justify-center h-screen text-black">
         Website Under Construction!
       </div>
-    </>
+
 
   )
 };
