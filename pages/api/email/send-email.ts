ebb4c5f028642f07ff307151278ad437e2b2848c
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-23 17:52:43 -0400

    add CRON job for notification service

--- pages/api/email/send-email.ts
+++ pages/api/email/send-email.ts
@@ -1,12 +1,49 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import axios from 'axios';
+import {Resend} from "resend";
 export default async function handler(req:NextApiRequest, res:NextApiResponse){
-  const data = await fetchManagementToken();
-  console.log(data);
-  res.status(200).json({token:data});
+  const resend = new Resend(process.env.RESEND_KEY);
+  const token = await fetchManagementToken();
+  const users  = await getUsers(token);
+  console.log(users['data']);
+  const emails:string[] = [];
+
+  for(let i = 0; i < users['data'].length; i++){
+    emails.push(users['data'][i].email);
+    console.log(users['data'][i].email);
+  }
+  console.log("outside for loop emails is", emails);
+  for(let i = 0; i< emails.length; i++){
+    console.log(emails[i]);
+    await resend.emails.send({
+      from:'reminder@tacompanion.com',
+      to: emails[i],
+      subject: 'Log your hours!',
+      html: '<p> This is your weekly reminder to log your hours in Workday! Visit  <strong>workday.wpi.edu</strong> to do so!</p>'
+    });
+    setTimeout(() => {  console.log('World!'); }, 2000);
+
+
+
+  }
+  res.status(200).json(emails);
 }
 
 
+async function getUsers(token:string){
+  console.log(token);
+  const queryOptions = {
+    method: 'GET',
+    url: 'https://dev-r7lqz0ea5g3p461d.us.auth0.com/api/v2/users',
+    headers: {
+      authorization: `Bearer ${token}`,
+    }
+  };
+  const data = await axios.request(queryOptions);
+  return data;
+
+
+}
 
 async function fetchManagementToken(){
   try{
@@ -22,8 +59,9 @@ async function fetchManagementToken(){
       }),
     };
     const data = await axios.request(options);
+    console.log("raw data is", data['data']['access_token']);
 
-    return data.data["access_token"];
+    return data['data']['access_token'];
   }
   catch(err){
     console.log(err);
