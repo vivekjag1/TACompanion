0454719d80aeaaff68ed4bf03cc30ac2e448366d
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-25 21:07:56 -0400

    getting closer to backend integration

--- pages/hours.tsx
+++ pages/hours.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Fullcalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -10,13 +10,85 @@ import type {HoursType} from "@/mongoose/timeWorked/schema";
 import {useUser} from "@auth0/nextjs-auth0/client";
 import {gql} from "graphql-tag";
 import client from "../graphql/client";
-
+import {useRouter} from "next/router";
 moment.tz.setDefault('America/New_York');
-
 const Hours = () =>{
   let { user, error, isLoading } = useUser();
+  const router = useRouter();
+
   const userName = user?.name;
-  const addCardMutation = gql `
+  const [open, setOpen] = useState<boolean>(false);
+  const [startTime, setStartTime] = useState<Date>(new Date());
+  const [hours, setHours] = useState<HoursType[]>([]);
+  const [date, setDate] = useState<string>("");
+  const [fetched, setFetched] = useState<boolean>(false);
+  const getHours = gql`
+     query getHoursByName($name:String){
+          fetchHoursByName(name:$name){
+              id
+              title 
+              courseCode
+              description
+              start 
+              end
+        }
+    }
+    `;
+
+  useEffect(() =>{
+    if(!user && !isLoading){ //if the information is fetched but there is still no user object then move to l
+      router.push('/api/auth/login').then();
+    }
+    else{
+      if(!fetched){
+        console.log("fetching data");
+        fetchData().then(console.log);
+      }
+      else{
+        return;
+      }
+
+    }
+  }, [user, isLoading, router, fetched]);
+
+
+  const fetchData = async () =>{
+    const data = await client.mutate({
+      mutation: getHours,
+      variables:{
+        name: user?.name
+      }
+    });
+    console.log("the data is", data);
+    const dataArr = data['data']['fetchHoursByName'];
+    const newItems = dataArr.map((item:HoursType) =>{
+      const toRet = {
+        title:item.title,
+        start:item.start,
+        emd: item.end,
+        color: (() =>{
+          if(item.type as string === "office hours"){
+            return 'blue';
+          }
+          else if (item.type as string === "meeting"){
+            return 'green';
+          }
+          else{
+            return 'red';
+          }
+
+        })
+      };
+      return toRet;
+    });
+    setHours(newItems);
+    setFetched(true);
+
+  }
+
+
+
+  const addHourMutation  = gql `
       mutation addHour($title:String, $courseCode:String, $description:String, $start:String, $end:String, $name:String){
           addHour(title: $title, courseCode: $courseCode,  description: $description, start: $start, end: $end, name: $name){
               id
@@ -28,10 +100,7 @@ const Hours = () =>{
           }
       }
   `;
-  const [open, setOpen] = useState<boolean>(false);
-  const [startTime, setStartTime] = useState<Date>(new Date());
-  const [hours, setHours] = useState<HoursType[]>([]);
-  const [date, setDate] = useState<string>("");
+
   const handleClick = (arg:DateClickArg) =>{
     setOpen(true);
     const index = arg.dateStr.indexOf("T");
@@ -59,13 +128,13 @@ const Hours = () =>{
         }
       })
     }
-    console.log("I jave just added a thing with the start date as", addToCalendar.start);
+    console.log("I jave just added a thing with the end date as", addToCalendar.end);
     setHours(prev => [...prev, addToCalendar]);
     //add graphql logic here to create an instance of the hours collection
 
     const executeMutation = async () =>{
       const data = await client.mutate({
-        mutation: addCardMutation,
+        mutation: addHourMutation,
         variables:{
           title: hour.title,
           courseCode: hour.courseCode,
@@ -77,11 +146,7 @@ const Hours = () =>{
       })
       return data;
     }
-
     executeMutation().then();
-
-
-
   }
   return(
     <>
