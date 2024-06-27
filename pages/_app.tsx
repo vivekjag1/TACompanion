c592f30811bf3b60067d6f4a6f98cb94af10f8ff
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-06-27 13:49:27 -0400

    add toast on modal submission

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,14 @@ import type { AppProps } from "next/app";
 import { useRouter } from 'next/router';
 import NavBar from "@/components/NavBar";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import { Toaster } from "@/components/ui/toaster"
-
+import {Toaster} from "sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const isHero = () => router.pathname === "/";
   return(
     <UserProvider>
+      <Toaster richColors/>
       {!isHero() && <NavBar/>}
       <Component {...pageProps}/>
     </UserProvider>
